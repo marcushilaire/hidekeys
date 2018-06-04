92f13d02013d2245e428898009bfa902a39464b3
Marcus Hilaire <marcushilaire@gmail.com>
2018-06-04 13:50:44 -0400

    Remembering file structure

---
+++ public/logic/logic.js
@@ -0,0 +1,87 @@
+console.log("linked");
+var searchTerms = [
+  "Greys Anatomy",
+  "My Hero Academia",
+  "Arrested Development",
+  "Cats",
+  "Dogs",
+  "Captain America",
+  "Brooklyn 99",
+  "The Office",
+  "Fraiser",
+  "Pickle Rick",
+  "Puppies",
+  "Pygmy Goats"
+];
+var buttonGenerate = function() {
+  $("#buttonsDiv").empty();
+  for (var i = 0; i < searchTerms.length; i++) {
+    var button = $("<button>")
+      .text(searchTerms[i])
+      .attr({
+        class: "terms btn",
+        "data-term": searchTerms[i]
+      });
+    $("#buttonsDiv").append(button);
+  }
+};
+console.log(apiKey);
+var fetchKey = function() {
+  $.ajax({
+    url: "apiKey",
+    data: {
+      apiKey
+    }
+  });
+};
+buttonGenerate();
+
+$("#addQuery").on("click", function(event) {
+  event.preventDefault();
+  var newQuery = $("#inputQuery").val();
+  if (newQuery !== "") {
+    searchTerms.push(newQuery);
+    buttonGenerate();
+  }
+});
+
+$(document).on("click", ".terms", function() {
+  $("#imgDump").empty();
+  var term = $(this).attr("data-term");
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?q=" +
+    term +
+    "&api_key=faVpcYpz55WtmrGnbEZLrtW06l9qyXdi&limit=10";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    var imageSet = response.data;
+    for (var i = 0; i < imageSet.length; i++) {
+      var div = $("<div>");
+      var rating = $("<p>").text("This gif is rated: " + imageSet[i].rating);
+      var img = $("<img>").attr({
+        src: imageSet[i].images.fixed_height_still.url,
+        class: "gifs img-responsive",
+        "data-still": imageSet[i].images.fixed_height_still.url,
+        "data-active": imageSet[i].images.fixed_height.url,
+        "data-state": "still"
+      });
+      div.append(rating, img).attr("class", "gifDiv");
+      $("#imgDump").append(div);
+    }
+  });
+});
+
+$(document).on("click", ".gifs", function() {
+  var state = $(this).attr("data-state");
+  var pause = $(this).attr("data-still");
+  var play = $(this).attr("data-active");
+  if (state == "still") {
+    $(this).attr("src", play);
+    $(this).attr("data-state", "active");
+  } else {
+    $(this).attr("src", pause);
+    $(this).attr("data-state", "still");
+  }
+});
--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 require("dotenv").config();
 var fs = require("fs");
 var keys = require("./keys.js");
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var fs = require("fs");
+var app = express();
 // importing the API object from keys.js
+var PORT = process.env.PORT || 8080;
 
 var apiKey = keys.API.apiKey;
-var appID = keys.API.appID;
+// var appID = keys.API.appID;
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-console.log(apiKey); // 1234
-console.log(appID); //5678
+app.get("/", function(req, res) {
+  //   res.send(apiKey);
+  console.log("loading home");
+  res.sendFile(path.join(__dirname, "./index.html"));
+  console.log(__dirname);
+});
+app.get("/apiKey", function(req, res) {
+  console.log("api key requested");
+  res.send(apiKey);
+});
+// console.log(apiKey); // 1234
+// console.log(appID); //5678
+app.listen(PORT, function() {
+  console.log("Server Running");
+});
