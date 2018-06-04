0eb253fbd9481cab2f02c5814ac413cfffa2bfbf
Marcus Hilaire <marcushilaire@gmail.com>
2018-06-04 12:01:43 -0400

    changes

--- keys.js
+++ keys.js
@@ -1,4 +1,6 @@
+// These are taken from .env and stored in the API object
 exports.API = {
   apiKey: process.env.key,
   appID: process.env.appID
 };
+//The API object is being exported and will be picked up by server.js
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 var fs = require("fs");
 var keys = require("./keys.js");
+// importing the API object from keys.js
+
 var apiKey = keys.API.apiKey;
 var appID = keys.API.appID;
 
-console.log(apiKey);
-console.log(appID);
+console.log(apiKey); // 1234
+console.log(appID); //5678
